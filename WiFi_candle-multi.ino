e55734852e75ab256ac4ceead511384808972caf
K1WIZ <john@wizworks.net>
2017-09-11 00:20:01 -0400

    Added SPIFFS Support

--- WiFi_candle-multi.ino
+++ WiFi_candle-multi.ino
@@ -3,6 +3,7 @@
 #include <WiFiClient.h>
 #include <ESP8266WebServer.h>
 #include <ESP8266mDNS.h>
+#include <FS.h>
 
 IPAddress ip(10, 2, 50, 100);
 IPAddress dns(10, 0, 1, 10);
@@ -40,27 +41,46 @@ void handleNotFound(){
  
 void setup(void){
   pinMode(fPin, OUTPUT);
+  pinMode(fPin2, OUTPUT);
   Serial.begin(115200);
+  SPIFFS.begin();
   WiFi.config(ip, dns, gateway, subnet);
   WiFi.begin(ssid, password);
   Serial.println("");
-
+/*
   // Wait for connection
   while (WiFi.status() != WL_CONNECTED) {
     delay(500);
   }
-  
+  */
   server.on("/", handleRoot);
   
   server.on("/on", [](){
   server.send(200, "text/plain", "Okay -- Candle is lit!");
   state = 1;
+  File f = SPIFFS.open("/state.txt", "w"); 
+
+    if (!f) {
+      Serial.println("file open failed on update.");
+    } else {
+      f.println(state); 
+      f.close();
+    }
+  
   });
   
   server.on("/off", [](){
   server.send(200, "text/plain", "Okay -- Candle is off!");
   //digitalWrite(fPin, 0);
   state = 0;
+  File f = SPIFFS.open("/state.txt", "w"); 
+
+    if (!f) {
+      Serial.println("file open failed on update.");
+    } else {
+      f.println(state); 
+      f.close();
+    }
   });
 
   server.onNotFound(handleNotFound);
@@ -70,12 +90,29 @@ void setup(void){
 }
  
 void loop(void){
+
+  File f = SPIFFS.open("/state.txt", "r");
+  if (!f) {
+      Serial.println("File open failed on read.");
+    } else {
+      while(f.available()) {
+        //Lets read line by line from the file
+        String line = f.readStringUntil('\n');
+        state = line.toInt();
+        break; //if left in, we'll just read the first line then break out of the while.
+      } 
+      f.close();
+    }
+
+  
   if (state == 1) {
   candle();
   }
   else {
+    if (state == 0) {
     analogWrite(fPin, 0);
     analogWrite(fPin2, 0);
+    }
   }
   server.handleClient();
 }
